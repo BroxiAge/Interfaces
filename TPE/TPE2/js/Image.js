632085615ec2c302bf191d1443f1f7e477b6d941
BroxiAge <nahuel.di.fiore@gmail.com>
2022-10-04 14:38:39 -0300

    saco filtro de mas

--- TPE/TPE2/js/Image.js
+++ TPE/TPE2/js/Image.js
@@ -9,7 +9,7 @@ document.getElementById('inp').onchange = function(e) {
     canvas.width = this.width;
     canvas.height = this.height;
     var ctx = canvas.getContext('2d');
-    ctx.filter = "saturate(8)";
+    // ctx.filter = "saturate(8)";
     ctx.drawImage(this, 0,0);
     
   }
